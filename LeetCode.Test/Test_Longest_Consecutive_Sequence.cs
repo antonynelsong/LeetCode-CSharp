f3d4dbb4bfa86643656f9a42b09055dbf9341dab
Antony Nelson G <antony.nelsong@siemens-healthineers.com>
2022-08-17 19:48:00 +0530

    add longest consecutive sequence

---
+++ LeetCode.Test/Test_Longest_Consecutive_Sequence.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Test
+{
+    public class Test_Longest_Consecutive_Sequence
+    {
+        [Test]
+        public void T01_Test_Longest_Consecutive_Sequence()
+        {
+            int[] nums = new int[] { 100, 4, 200, 1, 3, 2 };
+
+            Longest_Consecutive_Sequence sequence = new Longest_Consecutive_Sequence();
+
+            int actualResult = sequence.LongestConsecutive(nums);
+
+            Assert.That(actualResult, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void T02_Test_Longest_Consecutive_Sequence()
+        {
+            int[] nums = new int[] { 0, 3, 7, 2, 5, 8, 4, 6, 0, 1 };
+
+            Longest_Consecutive_Sequence sequence = new Longest_Consecutive_Sequence();
+
+            int actualResult = sequence.LongestConsecutive(nums);
+
+            Assert.That(actualResult, Is.EqualTo(9));
+        }
+
+        [Test]
+        public void T03_Test_Longest_Consecutive_Sequence()
+        {
+            int[] nums = new int[] { };
+
+            Longest_Consecutive_Sequence sequence = new Longest_Consecutive_Sequence();
+
+            int actualResult = sequence.LongestConsecutive(nums);
+
+            Assert.That(actualResult, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void T04_Test_Longest_Consecutive_Sequence()
+        {
+            int[] nums = new int[] { 9, 1, 4, 7, 3, -1, 0, 5, 8, -1, 6 };
+
+            Longest_Consecutive_Sequence sequence = new Longest_Consecutive_Sequence();
+
+            int actualResult = sequence.LongestConsecutive(nums);
+
+            Assert.That(actualResult, Is.EqualTo(7));
+        }
+
+        [Test]
+        public void T05_Test_Longest_Consecutive_Sequence()
+        {
+            int[] nums = new int[] { 1, 2, 0, 1 };
+
+            Longest_Consecutive_Sequence sequence = new Longest_Consecutive_Sequence();
+
+            int actualResult = sequence.LongestConsecutive(nums);
+
+            Assert.That(actualResult, Is.EqualTo(3));
+        }
+    }
+}
---
+++ LeetCode/Longest_Consecutive_Sequence.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class Longest_Consecutive_Sequence
+    {
+        public int LongestConsecutive(int[] nums)
+        {
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+           
+            nums = nums.Distinct().ToArray();
+            Array.Sort(nums);
+
+            int maxSize = 1;
+            int localcount = 1;
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i + 1] == nums[i] + 1)
+                {
+                    localcount++;
+                }
+                else
+                {
+                    localcount = 1;
+                }
+
+                if (maxSize < localcount)
+                {
+                    maxSize = localcount;
+                }
+            }
+            return maxSize;
+        }
+    }
+}
